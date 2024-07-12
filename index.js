3ca09f3798abdf23208d7d422cd65b92ccc82853
jagedos <jagedosusrecomm@gmail.com>
2024-07-12 15:59:10 +0300

    code grade completion

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let ride;
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!";
+  } else if(ride >= 400 && ride <= 2000){
+    return "That will be twenty bucks.";
+  } else if(ride> 2000 && ride <=2500){
+    return "I will gladly take your thirty bucks.";
+  } else{
+    return "No can do.";
+  }
+}
+scuberGreetingForFeet(199)
+
+
+// describe('switchOnCharmFromTip()', function () {
+//   it('should return "Thank you so much." if the tip is generous', function () {
+//     expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+//   });
+
+//   it('should return "Thank you." if the tip is not as generous', function () {
+//     expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+//   });
+
+//   it('should return "Bye." if anything else', function () {
+//     expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+//   });
+// });
+// });
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
